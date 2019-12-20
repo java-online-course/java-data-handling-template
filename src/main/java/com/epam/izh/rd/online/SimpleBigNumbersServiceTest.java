e7143d326cc143cc3fa2e8cd5d9317f77716f687
PKS <kirill_nn@inbox.ru>
2019-12-20 16:17:32 +0300

    +

---
+++ src/main/java/com/epam/izh/rd/online/SimpleBigNumbersServiceTest.java
@@ -0,0 +1,11 @@
+package com.epam.izh.rd.online;
+
+import com.epam.izh.rd.online.service.SimpleBigNumbersService;
+
+//TODO Удалить перед отправкой
+
+public class SimpleBigNumbersServiceTest {
+    public static void main(String[] args) {
+        SimpleBigNumbersService simpleBigNumbersService = new SimpleBigNumbersService();
+    }
+}
