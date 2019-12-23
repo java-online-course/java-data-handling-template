7e867b1a747f0464caf2687bfed77acc33ad1faa
PKS <kirill_nn@inbox.ru>
2019-12-23 12:23:39 +0300

    Наведение красоты, исправление ошибок, дополнительны проверки. Избыточные переменные по мнению IDEA оставил

--- src/main/java/com/epam/izh/rd/online/SimpleFileRepositoryTest.java
+++ src/main/java/com/epam/izh/rd/online/SimpleFileRepositoryTest.java
@@ -2,107 +2,31 @@ package com.epam.izh.rd.online;
 
 import com.epam.izh.rd.online.repository.SimpleFileRepository;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 public class SimpleFileRepositoryTest {
-    static int countFiles = 0;
-    static int countDir = 1;
-
     public static void main(String[] args) {
-
         SimpleFileRepository simpleFileRepository = new SimpleFileRepository();
+
         //Указываю путь к папке где у меня лежит проект и получаю как раз эти 10 файлов, но тесты не проходят!!
         //Что не так??
+        //Скорее всего дело в путях, что то напутали в относительных путях (Винда, *NIX), или напрочь забыли указать папку resources. ИМХО
+        //Ну или я где то налажал
         long files = simpleFileRepository.countFilesInDirectory("E:\\Git\\java-data-handling-template\\src\\main\\resources\\testDirCountFiles");
         System.out.println(files);
+
         //Аналогично, указываю путь к папке где у меня лежит проект и получаю как раз эти 7 папок, но тесты не проходят!!
         //Что не так??
-        //Скорее всего дело в путях, что то напутули в относительных путях, по моему напрочь забыли указать папку resources, в пути
+        //Скорее всего дело в путях, что то напутали в относительных путях (Винда, *NIX), или напрочь забыли указать папку resources. ИМХО
+        //Ну или я где то налажал
         long dir = simpleFileRepository.countDirsInDirectory("E:\\Git\\java-data-handling-template\\src\\main\\resources\\testDirCountFiles");
         System.out.println(dir);
-//
-//        System.out.println(countDir("F:\\"));
-//        copyOneFile("F:\\5\\6.txt","F:\\10\\6.txt");
-//        copyTXTFiles("F:\\10", "F:\\10C");
-//        simpleFileRepository.copyTXTFiles("F:\\10", "F:\\10C");
-//        simpleFileRepository.createFile("F:\\10C", "create");
-        System.out.println(simpleFileRepository.readFileFromResources("sensitive_data.txt"));
-    }
 
-    public static void copyTXTFiles(String dirFrom, String dirTo) {
-        File dir = new File(dirFrom + "\\");
-        File[] files = dir.listFiles();
-        assert files != null;
-        for (File need : files) {
-            if (need.isDirectory()) {
-                copyTXTFiles(need.getPath(), dirTo);
-            }
-            if (need.isFile()) {
-                if (need.toPath().toString().substring(need.toPath().toString().length() - 4).equalsIgnoreCase(".txt")) {
-                    System.out.println(need.toPath().toString());
-                    System.out.println(11);
-                    dirTo = dirTo + "\\";
-                    System.out.println(22);
-                    File newFile = new File(dirTo + need.getPath().substring((dirFrom + "\\").length()));
-                    System.out.println(newFile.toString());
-                    copyOneFile(need.toPath().toString(), newFile.toString());
-                }
-            }
-        }
+        //Пробовал на своей флешке - работает, в тестах вабще забыли про данный метод
+        simpleFileRepository.copyTXTFiles("D:\\2\\", null);
 
-    }
-
-    public static void copyOneFile(String from, String to) {
-        File source = new File(from);
-        File dest = new File(to);
-
-        try {
-            Files.copy(source.toPath(), dest.toPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void countPlus() {
-        countFiles++;
-    }
-
-    public static void countDirPlus() {
-        countDir++;
-    }
-
-    public static int countFiles(String path) {
-        File dir = new File(path);
-
-        File[] files = dir.listFiles();
-        assert files != null;
-        for (File need : files) {
-            if (need.isDirectory()) {
-                countFiles(need.getPath());
-            }
-            if (need.isFile()) {
-                countPlus();
-            }
-        }
-
-        return countFiles;
-    }
-
-    public static int countDir(String path) {
-        File dir = new File(path);
-
-        File[] files = dir.listFiles();
-        assert files != null;
-        for (File need : files) {
-            if (need.isDirectory()) {
-                countDirPlus();
-                countDir(need.getPath());
-            }
-
-        }
-
-        return countDir;
+        //Метод создает файл на диске с расширением txt, работает, но тест не проходит
+        System.out.println(simpleFileRepository.createFile("D:\\2", "txtfile_1"));
+        System.out.println(simpleFileRepository.createFile("D:\\2\\", "txtfile_2"));
+        System.out.println(simpleFileRepository.createFile("D:\\2\\", "txtfile_3.txt"));
+        System.out.println(simpleFileRepository.createFile("D:\\2", "txtfile_4.txt"));
     }
 }
--- src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 public class SimpleFileRepository implements FileRepository {
     static long countFiles = 0;
     static long countDir = 1; //1 для того, что бы сосчитать саму ROOT
+    final static String SEPARATOR = System.getProperty("file.separator");
+    final static String TYPE_TXT = ".txt";
 
     /**
      * Метод рекурсивно подсчитывает количество файлов в директории
@@ -17,12 +19,17 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countFilesInDirectory(String path) {
-        File dir = new File(path);
+        if (path == null || path.equals("")) {
+            return 0;
+        } //хотя может возвращать -1, что бы сразу бросалось в глаза??
 
+        File dir = new File(path + SEPARATOR);
         File[] files = dir.listFiles();
+
         if (files == null) {
             return 0;
         }
+
         for (File need : files) {
             if (need.isDirectory()) {
                 countFilesInDirectory(need.getPath());
@@ -31,6 +38,7 @@ public class SimpleFileRepository implements FileRepository {
                 countFiles++;
             }
         }
+
         return countFiles;
     }
 
@@ -42,12 +50,17 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countDirsInDirectory(String path) {
-        File dir = new File(path);
+        if (path == null || path.equals("")) {
+            return 0;
+        } //хотя может возвращать -1, что бы сразу бросалось в глаза??
+
+        File dir = new File(path + SEPARATOR);
 
         File[] files = dir.listFiles();
         if (files == null) {
             return 0;
         }
+
         for (File need : files) {
             if (need.isDirectory()) {
                 countDir++;
@@ -67,21 +80,38 @@ public class SimpleFileRepository implements FileRepository {
     //В тестах совсем забыли про тест этого метода
     @Override
     public void copyTXTFiles(String from, String to) {
-        File dir = new File(from + "\\");
+        if (from == null || to == null || from.equals("") || to.equals("")) {
+            return;
+        }
+
+        String dirFrom = "";
+        String dirTo = "";
+
+        if (from.endsWith(SEPARATOR)) {
+            dirFrom = from;
+        } else {
+            dirFrom = from + SEPARATOR;
+        }
+
+        if (to.endsWith(SEPARATOR)) {
+            dirTo = to;
+        } else {
+            dirTo = to + SEPARATOR;
+        }
+
+        File dir = new File(dirFrom);
         File[] files = dir.listFiles();
+
         assert files != null;
-        for (File need : files) {
+
+        for (File filePathTXT : files) {
             //В задании про подпапки нечего не сказано, да и работют, они в моей реализации не очень, файл папкаОткуда\Подпапка\файл.txt копируется в
-            // папкуКуда\файл.txt, подпапка не создается, как сделать правильно, пока ума не хватает
-            //поэтому подпапки закоментировал
-//            if (need.isDirectory()) {
-//                copyTXTFiles(need.getPath(), to);
-//            }
-            if (need.isFile()) {
-                if (need.toPath().toString().substring(need.toPath().toString().length() - 4).equalsIgnoreCase(".txt")) {
-                    to = to + "\\";
-                    File newFile = new File(to + need.getPath().substring((from + "\\").length()));
-                    copyOneFile(need.toPath().toString(), newFile.toString());
+            // папкуКуда\файл.txt, подпапка не создается, как сделать правильно, пока ума не хватает, поэтому подпапки не делал
+
+            if (filePathTXT.isFile()) {
+                if (filePathTXT.toPath().toString().substring(filePathTXT.toPath().toString().length() - 4).equalsIgnoreCase(TYPE_TXT)) {
+                    File newFile = new File(dirTo + filePathTXT.getPath().substring((dirFrom).length()));
+                    copyOneFile(filePathTXT.toPath().toString(), newFile.toString());
                 }
             }
         }
@@ -89,12 +119,24 @@ public class SimpleFileRepository implements FileRepository {
 
     //Не знаю можно или нет, но делаю доп метод
     void copyOneFile(String from, String to) {
-        File source = new File(from);
-        File dest = new File(to);
+        if (from == null || to == null || from.equals("") || to.equals("")) {
+            return;
+        }
+
+        File fileFrom = new File(from);
+        File fileTo = new File(to);
+
+        if (!fileFrom.exists()) {
+            return;
+        }
+
+        if (fileTo.exists()) {
+            return;
+        }
 
         try {
-            Files.copy(source.toPath(), dest.toPath());
-        } catch (IOException e) {
+            Files.copy(fileFrom.toPath(), fileTo.toPath());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -109,14 +151,38 @@ public class SimpleFileRepository implements FileRepository {
     //У меня файл создался, но тест не пройден
     @Override
     public boolean createFile(String path, String name) {
-        StringBuilder newFile = new StringBuilder();
-        newFile.append(path).append(System.getProperty("file.separator")).append(name).append(".txt");
-        File file = new File(newFile.toString());
+        if (name == null || name.equals("") || path == null || path.equals("")) {
+            return false;
+        }
+
+        String pathWithSeparator = "";
+
+        if (path.endsWith(SEPARATOR)) {
+            pathWithSeparator = path;
+        } else {
+            pathWithSeparator = path + SEPARATOR;
+        }
+
+        String filePathIS = "";
+
+        if (!name.endsWith(TYPE_TXT)) {
+            filePathIS = pathWithSeparator + name + TYPE_TXT;
+        } else {
+            filePathIS = path + name;
+        }
+
+        File file = new File(filePathIS);
+
+        if (file.exists()) {
+            return false;
+        } //не знаю, правильно или нет, но если файл существует, возвращаю false, т.к. СОЗДАНИЯ файла не было
+
         try {
             return file.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return false;
     }
 
@@ -126,17 +192,27 @@ public class SimpleFileRepository implements FileRepository {
      * @param fileName имя файла
      * @return контент
      */
-    //И опять, у меня метод работает, наверное, что то с проверкой
+    //Странно, но тест прошел, в своей SimpleFileRepositoryTest, не делаю
     @Override
     public String readFileFromResources(String fileName) {
-        //Дописал жесткую связку в src\main\resources
-        String pathIS = "src" + System.getProperty("file.separator") + "main" + System.getProperty("file.separator") + "resources"
-                + System.getProperty("file.separator") + fileName;
+        if (fileName == null || fileName.equals("")) {
+            return null;
+        }
+
+        String filePathIS = "";
+        //Дописал жесткую связку в src\main\resources + добявляю .txt, если не указали
+        if (!fileName.endsWith(TYPE_TXT)) {
+            filePathIS = "src" + SEPARATOR + "main" + SEPARATOR + "resources" + SEPARATOR + fileName + TYPE_TXT;
+        } else {
+            filePathIS = "src" + SEPARATOR + "main" + SEPARATOR + "resources" + SEPARATOR + fileName;
+        }
+
         try {
-            return new String(Files.readAllBytes(Paths.get(pathIS)));
+            return new String(Files.readAllBytes(Paths.get(filePathIS)));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return null;
     }
 }
