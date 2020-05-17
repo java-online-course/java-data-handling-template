0058ee7be547b2c93d07b4db940b649096bb5592
Alex Mironov <nomanyash@gmail.com>
2020-05-17 13:40:59 +0400

    Files

--- src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
@@ -1,5 +1,10 @@
 package com.epam.izh.rd.online.repository;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class SimpleFileRepository implements FileRepository {
 
     /**
@@ -8,9 +13,22 @@ public class SimpleFileRepository implements FileRepository {
      * @param path путь до директори
      * @return файлов, в том числе скрытых
      */
+    long counterOfFiles = 0;
     @Override
     public long countFilesInDirectory(String path) {
-        return 0;
+        if (!path.substring(0,3).equals("src")) path = "src" + File.separator + "main" + File.separator +"resources" + File.separator + path;
+        File files = new File(path);
+        File[] array = files.listFiles();
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                File file = array[i];
+                if (file.isFile()) counterOfFiles++;
+                if (file.isDirectory()) {
+                     countFilesInDirectory(file.getPath());
+                }
+            }
+        }
+        return counterOfFiles;
     }
 
     /**
@@ -19,9 +37,23 @@ public class SimpleFileRepository implements FileRepository {
      * @param path путь до директории
      * @return число папок
      */
+
+    long counterOfDirectories = 1;
     @Override
     public long countDirsInDirectory(String path) {
-        return 0;
+        if (!path.substring(0,3).equals("src")) path = "src" + File.separator + "main" + File.separator +"resources" + File.separator + path;
+        File files = new File(path);
+        File[] array = files.listFiles();
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                File file = array[i];
+                if (file.isDirectory()) {
+                    counterOfDirectories++;
+                    countDirsInDirectory(file.getPath());
+                }
+            }
+        }
+        return counterOfDirectories;
     }
 
     /**
@@ -32,7 +64,20 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public void copyTXTFiles(String from, String to) {
-        return;
+        File directory = new File(to);
+        File files = new File(from);
+        String[] names = files.list();
+        File[] paths = files.listFiles();
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].contains(".txt")) {
+                try {
+                    Files.copy(paths[i].toPath(), directory.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     /**
@@ -44,7 +89,15 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public boolean createFile(String path, String name) {
-        return false;
+        File file = new File (path);
+        file.mkdir();
+        File file1 = new File(path + File.separator + name);
+        try {
+            return file1.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 
     /**
@@ -55,6 +108,15 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public String readFileFromResources(String fileName) {
-        return null;
+        String s = "";
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("src\\main\\resources\\" + fileName))) {
+            s = reader.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s;
     }
 }
