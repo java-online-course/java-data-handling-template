c93a0105e319977b7931e99bbaaa8001b4b399b0
Angelika <oleinik38@gmail.com>
2020-05-27 16:28:30 +0300

    all done

--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
@@ -1,5 +1,14 @@
 package com.epam.izh.rd.online.service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleRegExpService implements RegExpService {
 
     /**
@@ -11,6 +20,22 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String maskSensitiveData() {
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/sensitive_data.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine(); //читаем
+            String regex = "(\\d{4}\\s)(\\d{4}\\s\\d{4})(\\s\\d{4})"; //что нужно найти
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(line); //ищем
+            while (matcher.find()) {
+                line = line.replaceAll(matcher.group(2), "**** ****"); //меняем
+            }
+            return line;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -22,6 +47,20 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String replacePlaceholders(double paymentAmount, double balance) {
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/sensitive_data.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine(); //читаем
+            BigDecimal payment = new BigDecimal(paymentAmount).setScale(0, RoundingMode.HALF_UP);
+            BigDecimal bal = new BigDecimal(balance).setScale(0, RoundingMode.HALF_UP);
+            line = line.replaceAll("(\\$\\{(payment_amount)})", String.valueOf(payment));
+            line = line.replaceAll("(\\$\\{(balance)})", String.valueOf(bal));
+            return line;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
@@ -4,61 +4,79 @@ public class SimpleTextService implements TextService {
 
     /**
      * Реализовать функционал удаления строки из другой строки.
-     *
+     * <p>
      * Например для базовой строки "Hello, hello, hello, how low?" и строки для удаления ", he"
      * метод вернет "Hellollollo, how low?"
      *
-     * @param base - базовая строка с текстом
+     * @param base   - базовая строка с текстом
      * @param remove - строка которую необходимо удалить
      */
     @Override
     public String removeString(String base, String remove) {
-        return null; //TODO
+        return base.replaceAll(remove, "");
     }
 
     /**
      * Реализовать функционал проверки на то, что строка заканчивается знаком вопроса.
-     *
+     * <p>
      * Например для строки "Hello, hello, hello, how low?" метод вернет true
      * Например для строки "Hello, hello, hello!" метод вернет false
      */
     @Override
     public boolean isQuestionString(String text) {
-        return false; //TODO
+        return text.endsWith("?");
     }
 
     /**
      * Реализовать функционал соединения переданных строк.
-     *
+     * <p>
      * Например для параметров {"Smells", " ", "Like", " ", "Teen", " ", "Spirit"}
      * метод вернет "Smells Like Teen Spirit"
      */
     @Override
     public String concatenate(String... elements) {
-        return null; //TODO
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : elements) {
+            stringBuilder.append(s);
+        }
+        return stringBuilder.toString();
     }
 
     /**
      * Реализовать функционал изменения регистра в вид лесенки.
      * Возвращаемый текст должен начинаться с прописного регистра.
-     *
+     * <p>
      * Например для строки "Load Up On Guns And Bring Your Friends"
      * метод вернет "lOaD Up oN GuNs aNd bRiNg yOuR FrIeNdS".
      */
     @Override
     public String toJumpCase(String text) {
-        return null; //TODO
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] textArray = text.toLowerCase().toCharArray();
+        for (int i = 0; i < textArray.length; i++) {
+            if (i % 2 != 0) {
+                stringBuilder.append(String.valueOf(textArray[i]).toUpperCase());
+            } else {
+                stringBuilder.append(textArray[i]);
+            }
+        }
+        return stringBuilder.toString();
     }
 
     /**
      * Метод определяет, является ли строка палиндромом.
-     *
+     * <p>
      * Палиндром - строка, которая одинаково читается слева направо и справа налево.
-     *
+     * <p>
      * Например для строки "а роза упала на лапу Азора" вернется true, а для "я не палиндром" false
      */
     @Override
     public boolean isPalindrome(String string) {
-       return false; //TODO
+        if (string.isEmpty()){
+            return false;
+        }
+        String s = string.toLowerCase().replaceAll(" ", "");
+        StringBuilder builder = new StringBuilder(s).reverse();
+        return s.equals(builder.toString());
     }
 }
