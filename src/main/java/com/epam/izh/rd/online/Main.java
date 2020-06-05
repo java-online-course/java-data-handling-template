91a3a6432f818753b377a3c6515b11af3dfb23e9
Alexander Savelyev <alexsave@gmail.com>
2020-06-05 10:43:07 +0300

    Class SimpleFileRepository is ready. One change has been added to the FileRepositoryTest.getFile

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,4 +1,17 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.repository.SimpleFileRepository;
+
 public class Main {
+
+    public static void main(String[] args) {
+        SimpleFileRepository simpleFileRepository = new SimpleFileRepository();
+        System.out.println(simpleFileRepository.countFilesInDirectory("c:/temp/java/java3"));
+        System.out.println(simpleFileRepository.countDirsInDirectory("c:/temp/java/java3/"));
+        simpleFileRepository.copyTXTFiles("c:/temp/java/java3/", "c:/temp/java/java4/");
+        //System.out.println(simpleFileRepository.createFile("c:/temp/java/java3/", "someNewFile.txt"));
+        System.out.println(simpleFileRepository.readFileFromResources("readme.txt"));
+    }
+
+
 }
