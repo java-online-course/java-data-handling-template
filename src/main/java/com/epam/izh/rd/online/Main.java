ff018f5c8f3d97d5fec27f4aca606cee9994e8d7
Alexander Savelyev <alexsave@gmail.com>
2020-06-05 17:32:46 +0300

    Class SimpleRegExpService is ready.

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -3,6 +3,7 @@ package com.epam.izh.rd.online;
 import com.epam.izh.rd.online.repository.SimpleFileRepository;
 import com.epam.izh.rd.online.service.SimpleBigNumbersService;
 import com.epam.izh.rd.online.service.SimpleDateService;
+import com.epam.izh.rd.online.service.SimpleRegExpService;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -22,13 +23,17 @@ public class Main {
 //        System.out.println(simpleBigNumbersService.getPrecisionNumber(1, 3, 2));
 //        System.out.println(simpleBigNumbersService.getPrimaryNumber(100));
 
-        SimpleDateService simpleDateService = new SimpleDateService();
-        System.out.println(simpleDateService.parseDate(LocalDate.of(1974, 2, 9)));
-        System.out.println(simpleDateService.parseString("1970-01-01 00:00"));
-        System.out.println(simpleDateService.convertToCustomFormat(LocalDate.of(1974, 2, 9), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-        System.out.println(simpleDateService.getNextLeapYear());
-        System.out.println(simpleDateService.getSecondsInYear(2021));
-        System.out.println(simpleDateService.getSecondsInYear(2020));
+//        SimpleDateService simpleDateService = new SimpleDateService();
+//        System.out.println(simpleDateService.parseDate(LocalDate.of(1974, 2, 9)));
+//        System.out.println(simpleDateService.parseString("1970-01-01 00:00"));
+//        System.out.println(simpleDateService.convertToCustomFormat(LocalDate.of(1974, 2, 9), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+//        System.out.println(simpleDateService.getNextLeapYear());
+//        System.out.println(simpleDateService.getSecondsInYear(2021));
+//        System.out.println(simpleDateService.getSecondsInYear(2020));
+
+        SimpleRegExpService simpleRegExpService = new SimpleRegExpService();
+        System.out.println(simpleRegExpService.maskSensitiveData());
+        System.out.println(simpleRegExpService.replacePlaceholders(1, 2));
     }
 
 
--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
@@ -1,5 +1,13 @@
 package com.epam.izh.rd.online.service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleRegExpService implements RegExpService {
 
     /**
@@ -11,6 +19,23 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String maskSensitiveData() {
+        try ( BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/sensitive_data.txt"))) {
+            Pattern pattern = Pattern.compile("\\d{4} \\d{4} \\d{4} \\d{4}");
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    String maskedReplacement = matcher.group().substring(0, 5) +
+                            "**** ****" + matcher.group().substring(14, 19);
+                    line = line.replace(matcher.group(), maskedReplacement);
+                }
+                stringBuilder.append("\n" + line);
+            }
+            return stringBuilder.toString().substring(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -22,6 +47,18 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String replacePlaceholders(double paymentAmount, double balance) {
+        try ( BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/sensitive_data.txt"))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replaceAll("\\$\\{payment_amount}",String.valueOf((int) paymentAmount));
+                line = line.replaceAll("\\$\\{balance}",String.valueOf((int) balance));
+                stringBuilder.append("\n" + line);
+            }
+            return stringBuilder.toString().substring(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
