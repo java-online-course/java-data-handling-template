6f7022032c1f7cd4b9c2756938895a7533b0e83b
Ivan_Babushkin <babushkinnavi.ib@gmail.com>
2020-10-21 22:41:48 +0300

    All methods are implemented and passed

--- src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
@@ -1,5 +1,8 @@
 package com.epam.izh.rd.online.repository;
 
+import java.io.*;
+import java.nio.file.Files;
+
 public class SimpleFileRepository implements FileRepository {
 
     /**
@@ -10,7 +13,17 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countFilesInDirectory(String path) {
-        return 0;
+        File filesInDir = new File("src/main/resources/" + path);
+        File[] files = filesInDir.listFiles();
+        long count = 0;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                count = countFilesInDirectory(path + "/" + file.getName()) + count;
+            } else {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -21,7 +34,15 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countDirsInDirectory(String path) {
-        return 0;
+        File filesInDir = new File("src/main/resources/" + path);
+        File[] files = filesInDir.listFiles();
+        long count = 1;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                count = countDirsInDirectory(path + "/" + file.getName()) + count;
+            }
+        }
+        return count;
     }
 
     /**
@@ -32,7 +53,21 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public void copyTXTFiles(String from, String to) {
-        return;
+        File fileTo = new File(to);
+        File fileFrom = new File(from);
+        File fileToPath = new File(fileTo.getParent());
+        if (!fileToPath.exists()) {
+            fileToPath.mkdirs();
+        }
+        for (File file : fileFrom.getParentFile().listFiles()) {
+            if (file.getName().endsWith(".txt")) {
+                try {
+                    Files.copy(file.toPath(), fileTo.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     /**
@@ -44,8 +79,19 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public boolean createFile(String path, String name) {
+        File file = new File(path);
+        File filePath = new File(file.getAbsolutePath());
+        if(!filePath.exists()){
+            file.mkdirs();
+        }
+        try {
+            return file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
+
 
     /**
      * Метод считывает тело файла .txt из папки src/main/resources
@@ -55,6 +101,17 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public String readFileFromResources(String fileName) {
-        return null;
+        StringBuilder textFromFile = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("src/main/resources/" + fileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                textFromFile.append(line);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return textFromFile.toString();
     }
 }
--- src/main/java/com/epam/izh/rd/online/service/SimpleDateService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleDateService.java
@@ -2,6 +2,7 @@ package com.epam.izh.rd.online.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Year;
 import java.time.format.DateTimeFormatter;
 
 public class SimpleDateService implements DateService {
@@ -14,7 +15,8 @@ public class SimpleDateService implements DateService {
      */
     @Override
     public String parseDate(LocalDate localDate) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return formatter.format(localDate);
     }
 
     /**
@@ -25,7 +27,8 @@ public class SimpleDateService implements DateService {
      */
     @Override
     public LocalDateTime parseString(String string) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return LocalDateTime.parse(string, formatter);
     }
 
     /**
@@ -37,7 +40,7 @@ public class SimpleDateService implements DateService {
      */
     @Override
     public String convertToCustomFormat(LocalDate localDate, DateTimeFormatter formatter) {
-        return null;
+        return formatter.format(localDate);
     }
 
     /**
@@ -47,7 +50,11 @@ public class SimpleDateService implements DateService {
      */
     @Override
     public long getNextLeapYear() {
-        return 0;
+        int year = LocalDate.now().getYear();
+        do {
+            year++;
+        } while (!Year.of(year).isLeap());
+        return year;
     }
 
     /**
@@ -57,8 +64,6 @@ public class SimpleDateService implements DateService {
      */
     @Override
     public long getSecondsInYear(int year) {
-        return 0;
+        return Year.of(year).length() * 60 * 60 * 24;
     }
-
-
 }
