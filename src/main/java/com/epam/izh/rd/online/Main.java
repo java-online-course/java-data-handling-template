95152f159c90f374a3fe70c345b089274064d6a5
Evgeniy <nifpopnaf@gmail.com>
2021-10-28 17:43:32 +0400

    add TextService

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,4 +1,10 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.service.SimpleTextService;
+
 public class Main {
+    public static void main(String[] args) {
+        SimpleTextService sm = new SimpleTextService();
+        System.out.println(sm.isPalindrome(null));
+    }
 }
--- src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
@@ -1,64 +1,89 @@
 package com.epam.izh.rd.online.service;
 
+import java.util.Locale;
+
 public class SimpleTextService implements TextService {
 
     /**
      * Реализовать функционал удаления строки из другой строки.
-     *
+     * <p>
      * Например для базовой строки "Hello, hello, hello, how low?" и строки для удаления ", he"
      * метод вернет "Hellollollo, how low?"
      *
-     * @param base - базовая строка с текстом
+     * @param base   - базовая строка с текстом
      * @param remove - строка которую необходимо удалить
      */
     @Override
     public String removeString(String base, String remove) {
-        return null; //TODO
+        if ((base == null || base.isEmpty())
+                || (remove == null || remove.isEmpty())) {
+            return null;
+        }
+        return base.replace(remove, "");
     }
 
     /**
      * Реализовать функционал проверки на то, что строка заканчивается знаком вопроса.
-     *
+     * <p>
      * Например для строки "Hello, hello, hello, how low?" метод вернет true
      * Например для строки "Hello, hello, hello!" метод вернет false
      */
     @Override
     public boolean isQuestionString(String text) {
-        return false; //TODO
+        if (text == null || text.isEmpty()) {
+            return false;
+        }
+
+        return text.endsWith("?");
     }
 
     /**
      * Реализовать функционал соединения переданных строк.
-     *
+     * <p>
      * Например для параметров {"Smells", " ", "Like", " ", "Teen", " ", "Spirit"}
      * метод вернет "Smells Like Teen Spirit"
      */
     @Override
     public String concatenate(String... elements) {
-        return null; //TODO
+        StringBuilder sb = new StringBuilder();
+        for (String st : elements) {
+            sb.append(st);
+        }
+        return sb.toString();
     }
 
     /**
      * Реализовать функционал изменения регистра в вид лесенки.
      * Возвращаемый текст должен начинаться с прописного регистра.
-     *
+     * <p>
      * Например для строки "Load Up On Guns And Bring Your Friends"
      * метод вернет "lOaD Up oN GuNs aNd bRiNg yOuR FrIeNdS".
      */
     @Override
     public String toJumpCase(String text) {
-        return null; //TODO
+        StringBuilder sb = new StringBuilder();
+        text = text.toLowerCase(Locale.ROOT);
+        for (int i = 0; i < text.length(); i++) {
+            if (i % 2 != 0) {
+                sb.append(Character.toUpperCase(text.charAt(i)));
+            }
+        }
+        return sb.toString();
     }
 
     /**
      * Метод определяет, является ли строка палиндромом.
-     *
+     * <p>
      * Палиндром - строка, которая одинаково читается слева направо и справа налево.
-     *
+     * <p>
      * Например для строки "а роза упала на лапу Азора" вернется true, а для "я не палиндром" false
      */
     @Override
     public boolean isPalindrome(String string) {
-       return false; //TODO
+        if (string == null || string.isEmpty()) {
+            return false;
+        }
+        string = string.replace(" ", "").toLowerCase(Locale.ROOT);
+        return string.equals(new StringBuilder(string).reverse().toString());
     }
 }
