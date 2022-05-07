8744ded019791054660b89ab0030ab7c231f3df1
Dmitrii Reshetnikov <chipsoft2@gmail.com>
2022-05-07 22:51:35 +0400

    BigNumbers complete

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,4 +1,14 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.service.SimpleBigNumbersService;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
+    public static void main(String[] args) {
+        SimpleBigNumbersService simpleBigNumbersService = new SimpleBigNumbersService();
+        System.out.println(simpleBigNumbersService.getPrimaryNumber(101));
+        System.out.println(simpleBigNumbersService.getPrecisionNumber(1,3,2));
+    }
 }
